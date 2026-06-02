e6c3d132ef1bbb6968df50f9d6852d48bac2c6e1
Dharmendra Kumar <dharmendra9847@gmail.com>
2026-06-02 07:04:30 +0530

    Implement dependency injection using @Autowired annotation in Spring

---
+++ Autowired-annotation/src/main/java/com/spring/autowired/annotation/Employee.java
@@ -0,0 +1,64 @@
+package com.spring.autowired.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("employee")
+public class Employee {
+
+    @Value("1")
+    private int employeeId;
+
+    @Value("Alice")
+    private String employeeName;
+
+    @Value("alice@gmail.com")
+    private String employeeEmailAddress;
+
+    @Value("10000")
+    private double salary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeEmailAddress() {
+        return employeeEmailAddress;
+    }
+
+    public void setEmployeeEmailAddress(String employeeEmailAddress) {
+        this.employeeEmailAddress = employeeEmailAddress;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String displayEmployeeDetails() {
+        return "-----------------------------------\n" +
+                "Employee Details:\n" +
+                "-----------------------------------\n" +
+                "ID            : " + employeeId + "\n" +
+                "Name          : " + employeeName + "\n" +
+                "Email         : " + employeeEmailAddress + "\n" +
+                "Salary        : $" + salary + "\n" +
+                "-----------------------------------";
+    }
+
+}
---
+++ Autowired-annotation/src/main/java/com/spring/autowired/annotation/Manager.java
@@ -0,0 +1,20 @@
+package com.spring.autowired.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Manager {
+    private Employee employee;
+
+    @Autowired
+    public Manager(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public String toString() {
+        return "\n=== Manager ===\n" + employee.displayEmployeeDetails() +
+                '}';
+    }
+}
